2d04eddbf2fa5f5282fa2bcae900778d64f6077e
kitten-code <77833405+kitten-code@users.noreply.github.com>
2021-12-29 20:30:50 +0100

    Add toolbar

--- src/bank/MainView.java
+++ src/bank/MainView.java
@@ -2,10 +2,33 @@ package bank;
 
 import javax.swing.*;
 
-public class MainView  extends JFrame {
+public class MainView extends JFrame {
     private JPanel MainPanel;
+    private JToolBar MainToolBar;
 
-    public MainView()  {
+    // Klient
+    // id
+    // imie
+    // nazwisko
+    // płeć
+    // data_urodzenia
+    // id_rachunku
+
+    // Rachunek
+    // id
+    // numer_konta
+    // stan_konta => 220
+    // data_wpłaty => data wpłaty 20 pln
+    // data_Wyplaty
+
+    // Historia
+    // id
+    // id_rachunku
+    // typ: wpłata/wypłata
+    // kwota
+
+
+    public MainView() {
         super("MainView");
         this.setContentPane(this.MainPanel);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
