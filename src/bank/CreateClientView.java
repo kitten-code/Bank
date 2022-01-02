8ea6992370be0241f4cb9afad32463d51eebd173
kitten-code <77833405+kitten-code@users.noreply.github.com>
2022-01-02 12:21:59 +0100

    Add form client part 1

--- src/bank/CreateClientView.java
+++ src/bank/CreateClientView.java
@@ -1,4 +1,25 @@
 package bank;
 
-public class CreateClientView {
+import javax.swing.*;
+
+public class CreateClientView extends JFrame {
+    private JTextField textFieldName;
+    private JTextField textFieldSurname;
+    private JTextField textFieldDateBirth;
+    private JTextField textFieldAccountNumber;
+    private JPanel mainPanel;
+    private JLabel nameLabel;
+    private JLabel surnameLabel;
+    private JLabel birthDateLabel;
+    private JLabel AccountNumberLabel;
+    private JLabel genderLabel;
+    private JRadioButton femaleRadioButton;
+    private JRadioButton maleRadioButton;
+
+
+    public CreateClientView (){
+      super("MainView");
+      this.setContentPane(this.mainPanel); // komponent poczatkowy
+      this.pack();
+  }
 }
