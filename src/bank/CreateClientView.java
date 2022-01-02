1e41c3bb426b7fb286eb7dc2e6145a1361e3c5ad
kitten-code <77833405+kitten-code@users.noreply.github.com>
2022-01-02 13:54:27 +0100

    Add fields validation on click

--- src/bank/CreateClientView.java
+++ src/bank/CreateClientView.java
@@ -1,6 +1,12 @@
 package bank;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class CreateClientView extends JFrame {
     private JTextField textFieldName;
@@ -27,9 +33,59 @@ public class CreateClientView extends JFrame {
     private JButton closeButton;
 
 
-    public CreateClientView (){
-      super("Dodawanie Klienta");
-      this.setContentPane(this.mainPanel); // komponent poczatkowy
-      this.pack();
-  }
+    public CreateClientView() {
+        super("Dodawanie Klienta");
+        this.setContentPane(this.mainPanel); // komponent poczatkowy
+        this.pack();
+
+        addPaymentPanel.setVisible(false);
+        yesCheckBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (yesCheckBox.isSelected()) {
+                    addPaymentPanel.setVisible(true);
+                }
+                if (!yesCheckBox.isSelected()) {
+                    addPaymentPanel.setVisible(false);
+                }
+            }
+        });
+
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (allFieldsAreValid()) {
+                    System.out.println("Dodaje Ola ja kilentów");
+                }
+            }
+        });
+    }
+
+    private boolean allFieldsAreValid() {
+        if (areFieldsEmpty() || !areFormatsValid()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean areFieldsEmpty() {
+        if (textFieldName.getText().equals("") || textFieldSurname.getText().equals("") || textFieldDateBirth.getText().equals("") || textFieldAccountNumber.getText().equals("")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean areFormatsValid() {
+        try {
+            LocalDate date = LocalDate.parse(textFieldDateBirth.getText(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            int accountNumber = Integer.parseInt(textFieldAccountNumber.getText());
+        } catch (Exception exception) {
+            return false;
+        }
+// jesli format jest niepoprawny to metoda parse wyrzuca wyjątek który łapiemy i wtedy zwracmy fałsz
+// jeśli wyjątek nie zostanie wyrzucony to znaczy że formaty są poprawne i zwracamy prawde czyli areFormatsValid() = true
+        return true;
+    }
 }
