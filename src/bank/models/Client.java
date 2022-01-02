3671556ad6b4cbfb3c2c0704caf865221495ee69
kitten-code <77833405+kitten-code@users.noreply.github.com>
2022-01-02 12:42:33 +0100

    Add client class and gender enum

---
+++ src/bank/models/Client.java
@@ -0,0 +1,11 @@
+package bank.models;
+
+import java.util.Date;
+
+public class Client {
+    int id;
+    String name;
+    String surname;
+    Date birthDate;
+    Gender gender;
+}
---
+++ src/bank/models/Gender.java
@@ -0,0 +1,5 @@
+package bank.models;
+
+public enum Gender {
+    Male, Female
+}
